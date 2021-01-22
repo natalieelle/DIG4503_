31a813c04e43a381785eec59533697cb5d23f4a6
Natalie Laurent <nlaurent1222@gmail.com>
2021-01-22 16:56:54 -0500

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,12 @@
+class Person {
+  var name = "";
+  var favoriteColor = "";
+  
+  function constructor() {
+    }
+  function speak() {
+    consolel.log(name + favoriteColor);
+    //This function should use console.log() to show the person's name in their favorite color
+
+  }
+}
