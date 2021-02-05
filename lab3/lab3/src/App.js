6d9603729d4caba0bca4643a616b55984c4d09cc
Natalie <nlaurent1222@gmail.com>
2021-02-05 15:12:02 -0500

    Lab 3

---
+++ lab3/lab3/src/App.js
@@ -0,0 +1,14 @@
+
+import React from './react';
+import HomePage from '../components/homepage.js/homepage'
+
+function App() {
+  const fname = 'Natalie';
+
+  return (
+    <Homepage firstname={fname} />
+  )
+    
+}
+
+export default App;
