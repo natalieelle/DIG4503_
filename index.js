66ffed9ad8dee8c163fc9a17dd3e47999481d57a
Natalie <nlaurent1222@gmail.com>
2021-02-05 08:27:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const customer = {
 
 //              your code inside these <div> tags
 
-const output = <div></div>;
+const output = (
+<div>
+    <h1>My name is {customer.first_name}</h1>
+    <h2>My last name is {customer.last_name}</h2>
+  </div> 
+  );
 
 ReactDOM.render(output, document.querySelector("#root"));
