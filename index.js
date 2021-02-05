059af61d137b5f3b5baf045daf660fdbf563ae38
Natalie Laurent <nlaurent1222@gmail.com>
2021-02-05 08:03:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const example = require("Natalie.js");
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const customer = {
+
+  first_name: "Alex",
+
+  last_name: "DeVille"
+
+};
+
+//              your code inside these <div> tags
+
+const output = <div></div>;
+
+ReactDOM.render(output, document.querySelector("#root"));
