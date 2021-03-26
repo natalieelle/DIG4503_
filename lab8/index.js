60f5f8abd4033fc7da47ff7563979052dd5a293b
Natalie <nlaurent1222@gmail.com>
2021-03-26 09:54:11 -0400

    Lab9

--- lab8/index.js
+++ lab8/index.js
@@ -7,20 +7,47 @@ let fileContents = fs.readFileSync("database.json");
 
 let database = JSON.parse(fileContents);
 
+App.use("/", Express.static("public"));
+
 App.get("employees/:name", (req, res) => {
-    let result = { error: "not found"};
+    let result = {name: "name", age: "#"};
 
     database.forEach((value) => {
         if(req.params.name == value.name) {
         result = value;
 
     } else {
-        res.json({name: "name", age: "#"});
+        res.json({ error: "not found"});
     }
 });
 res.json(result);
 })
 
+App.get("ages/:number", (req, res) => {
+    let result = {name: "name", age: "#"};
+
+    database.forEach((value) => {
+        if(req.params.number == value.number) {
+        result = value;
+
+    } else {
+        res.json({ error: "not found"});
+    } 
+});
+res.json(result);
+})
+
+App.post("employee/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name, 
+        "age": parseInt(req.params.age)
+    };
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
 
 App.listen(port, () => {
     console.log.log("Server running!");
